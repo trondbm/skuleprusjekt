779808b26d80bc0ee6e052e74266100f76dbaad4
Sigurd Bentås <sigurben@student.hist.no>
2016-03-08 12:07:43 +0100

    Hallo

--- Prosjektet/Prosjektet/Form1.vb
+++ Prosjektet/Prosjektet/Form1.vb
@@ -7,6 +7,6 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.Close()
+        Application.Exit()
     End Sub
 End Class
