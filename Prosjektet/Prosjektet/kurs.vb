e704445dae00ee31461ede5746468b92a1a7fc1a
Trond Bongard Munkeby <trond.bongard@hotmail.com>
2016-04-12 22:16:02 +0200

    LOL

--- Prosjektet/Prosjektet/kurs.vb
+++ Prosjektet/Prosjektet/kurs.vb
@@ -7,6 +7,61 @@ Public Class kurs
         Me.Text = "Kurs"
         db.DBConnect()
 
+        '  Dim reader As MySqlDataReader
+
+        '  Try
+        '    Dim sqlkurs As String
+        '    sqlkurs = "SELECT * FROM 'VARE'"
+        '   Dim Command = New MySqlCommand(sqlkurs, con)
+        '  reader = Command.executereader
+        '   While reader.Read
+        '          Dim vname = reader.GetString("varenavn")
+        '    ListBox1.Items.Add(vname)
+        '       End While
+
+
+
+        '    Catch ex As Exception
+
+        '    End Try
+
+
+        Dim query As String = "SELECT varenavn FROM VARE"
+        Dim sqlkurs As New MySqlCommand(query, con)
+
+
+
+        'Create a command object with the SQL statement needed to select the first and last names
+        Dim strSQL AsString = "SELECT FirstName, LastName FROM Employees"
+      Dim objCommand AsNew OleDbCommand(strSQL, objConnection)
+ 
+      'Create a data adapter and data table then fill the data table
+      Dim objDataAdapter AsNew OleDbDataAdapter(objCommand)
+      Dim objDataTable AsNew DataTable("Employees")
+      objDataAdapter.Fill(objDataTable)
+
+        'Create connection and release resources
+        objConnection.Close()
+        objConnection.Dispose()
+        objConnection = Nothing
+        objCommand.Dispose()
+        objCommand = Nothing
+        objDataAdapter.Dispose()
+        objDataAdapter = Nothing
+
+        'Fill names into the listbox
+        ForEach row As DataRow In objDataTable.Rows
+           lstNames.Items.Add(row.Item("FirstName") & " " & row.Item("LastName"))
+        Next
+
+        'Release resources
+        objDataTable.Dispose()
+        objDataTable = Nothing
+        EndSub
+
+
+
+
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
@@ -17,5 +72,9 @@ Public Class kurs
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Meny.Show()
         Close()
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
     End Sub
 End Class
