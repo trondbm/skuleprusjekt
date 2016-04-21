db121ef40b8b98f4eefb55fd80d90c15f25b78b3
Trond Bongard Munkeby <trond.bongard@hotmail.com>
2016-04-21 10:32:58 +0200

    d

---
+++ Prosjektet/Prosjektet/Mainforms/lager.vb
@@ -0,0 +1,162 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class lager
+
+    Dim db As New DBConnect
+    Dim dataset As New DataTable
+    Dim sda As New MySqlDataAdapter
+    Dim bsource As New BindingSource
+    Dim vare As String
+    Dim lager As String
+
+    Public Sub load_table()
+        DataGridView1.DataSource = dataset
+        DataGridView1.DataSource = Nothing
+        dataset.Clear()
+        Try
+            Dim query As String
+            query = "Select * from VARE"
+            Dim sqlsøk = New MySqlCommand(query, con)
+            sda.SelectCommand = sqlsøk
+            sda.Fill(dataset)
+            bsource.DataSource = dataset
+            DataGridView1.DataSource = bsource
+            sda.Update(dataset)
+
+        Catch ex As MySqlException
+            MsgBox("Error")
+        End Try
+    End Sub
+
+    Public Sub load_location()
+
+        DataGridView1.DataSource = dataset
+        DataGridView1.DataSource = Nothing
+        dataset.Clear()
+        Try
+            Dim query As String
+            query = "Select * from VARE where lager = '" + lager + "'"
+            Dim sqlsøk = New MySqlCommand(query, con)
+            sda.SelectCommand = sqlsøk
+            sda.Fill(dataset)
+            bsource.DataSource = dataset
+            DataGridView1.DataSource = bsource
+            sda.Update(dataset)
+
+        Catch ex As MySqlException
+            MsgBox("Error")
+        End Try
+    End Sub
+
+    Private Sub kurs_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Text = "Lager"
+        db.DBConnect()
+        load_table()
+
+
+
+
+    End Sub
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Vareregistrering.Show()
+        AddHandler leggtilkurs.FormClosed, AddressOf load_table
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Meny.Show()
+        Close()
+    End Sub
+
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+
+
+
+        vare = InputBox("Hvilken kurs_id vil du slette?", "Slett kurs")
+
+        If vare = "" Then
+            load_table()
+        ElseIf IsNumeric(vare) = False Then
+            MsgBox("Du må skrive et tall!", MsgBoxStyle.Critical, "Error")
+        ElseIf vare
+
+            Try
+                Dim sqlkurs = New MySqlCommand("DELETE FROM VARE WHERE vareID = " & vare, con)
+
+
+
+                sqlkurs.ExecuteNonQuery()
+
+            Catch ex As MySqlException
+
+            End Try
+
+            load_table()
+
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+        AddHandler endrekurs.FormClosed, AddressOf load_table
+        '  kurs = InputBox("Hvilken kurs_id vil du endre?")
+        '  endrevare.Show()
+
+
+    End Sub
+
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        DataGridView1.DataSource = dataset
+        DataGridView1.DataSource = Nothing
+        dataset.Clear()
+        load_table()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        lager = "Trondheim"
+
+        load_location()
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        lager = "Oslo"
+
+        load_location()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        lager = "Bodø"
+
+        load_location()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        lager = "Bergen"
+
+        load_location()
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        lager = "Stavanger"
+
+        load_location()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        lager = "Tromsø"
+
+        load_location()
+    End Sub
+End Class
